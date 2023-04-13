3338b1212bc4bf2dee356f40b699c24634a79832
Antonio Martinez <antonio_martinez88@hotmail.com>
2023-04-13 16:10:28 -0400

    updated

--- 2_regression/1_simple_linear_regression/simple_linear_regression.py
+++ 2_regression/1_simple_linear_regression/simple_linear_regression.py
@@ -12,15 +12,17 @@ y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Training the Simple Linear Regression model on the Training set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
-regressor.fit(X_train, y_train)
+regressor.fit(X_train, y_train) # adjustment/fit method to train de model with linear regression
 
-# Predicting the Test set results
-y_pred = regressor.predict(X_test)
+# Predicting the Test set results (return a vector prediction)
+y_pred = regressor.predict(X_test) #for the test will try to predict salary based on number of years experience
+print("X_test: \n", X_test)
+print("y_prediction: \n",y_pred) # is returning points over the line because is a line actually
 
 # Visualising the Training set results
 plt.scatter(X_train, y_train, color = 'red')
@@ -31,8 +33,8 @@ plt.ylabel('Salary')
 plt.show()
 
 # Visualising the Test set results
-plt.scatter(X_test, y_test, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.scatter(X_test, y_test, color = 'red') #red points will be associated with the real observation data
+plt.plot(X_train, regressor.predict(X_train), color = 'blue') #blue line is the regression prediction of ML
 plt.title('Salary vs Experience (Test set)')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
