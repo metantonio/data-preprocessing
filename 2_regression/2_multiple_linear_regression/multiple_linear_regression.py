9e62810c23bfcaba39599ac24df7dac5f331d439
Antonio Martinez <antonio_martinez88@hotmail.com>
2023-04-14 09:02:18 -0400

    upd

--- 2_regression/2_multiple_linear_regression/multiple_linear_regression.py
+++ 2_regression/2_multiple_linear_regression/multiple_linear_regression.py
@@ -17,7 +17,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough') #remember change the index of column if there are a categorical data
 X = np.array(ct.fit_transform(X)) #on multiple linear regression we don't need apply scaling features
-print(X)
+print("transformed matrix of features: \n",X)
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split 
@@ -30,5 +30,13 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
-np.set_printoptions(precision=2)
-print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+np.set_printoptions(precision=2) # show only 2 decimals for any numerical impresion
+
+# We can concatenate vertically and/or horizantlly 2 vectors with np.concatenate. Con reshape podemos mostrar una cierta longitud del vector en vertical en lugar de horizontal
+# So, we can comparate the prection vector with the actual values of the test vector to validate our model
+# the second parameter of paramenter of .concatenate equal to 1 will make horizontal concatenation
+print("comparation: \n",np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+# Making a single prediction of Profit (for example the profit of a startup with R&D Spend = 160000, Administration Spend = 130000, Marketing Spend = 300000 and State = 'California')
+print("\nA single prediction: \n",regressor.predict([[1, 0, 0, 160000, 130000, 300000]])) # The One Hot Enconder made [1,0,0] for California
+
