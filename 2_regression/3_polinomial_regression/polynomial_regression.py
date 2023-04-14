5cf69460b9a1e71e8fe36cc9f1e5db8e69cf6c6b
Antonio Martinez <antonio_martinez88@hotmail.com>
2023-04-14 09:44:54 -0400

    upd

--- 2_regression/3_polinomial_regression/polynomial_regression.py
+++ 2_regression/3_polinomial_regression/polynomial_regression.py
@@ -7,8 +7,9 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, 1:-1].values
+X = dataset.iloc[:, 1:-1].values # As position is related with Level, we don't really need position column
 y = dataset.iloc[:, -1].values
+print("dataset X:\n",X)
 
 # Training the Linear Regression model on the whole dataset
 from sklearn.linear_model import LinearRegression
@@ -17,10 +18,11 @@ lin_reg.fit(X, y)
 
 # Training the Polynomial Regression model on the whole dataset
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree = 4)
-X_poly = poly_reg.fit_transform(X)
-lin_reg_2 = LinearRegression()
-lin_reg_2.fit(X_poly, y)
+poly_reg = PolynomialFeatures(degree = 4) # Matrix of powered features, until n=4 (for n higher, more precision)
+X_poly = poly_reg.fit_transform(X) #transform the matrix of features into a matrix of square feature
+print("transformed dataset X_poly:\n",X_poly)
+lin_reg_2 = LinearRegression() # Actually, polinomial regression is a particular case of multiple lineal regression, where x2 = x1^2, x3=x1^3, etc...
+lin_reg_2.fit(X_poly, y) # Training of polynomial regression model
 
 # Visualising the Linear Regression results
 plt.scatter(X, y, color = 'red')
