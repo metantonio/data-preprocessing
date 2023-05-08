a4bb573b029b6b2f2719f8c2890f9fcbc276fabf
Antonio Martinez <antonio_martinez88@hotmail.com>
2023-05-08 12:34:24 -0400

    upd

--- 7_natural_language_processing/Section 36 - Natural Language Processing/Python/natural_language_processing.py
+++ 7_natural_language_processing/Section 36 - Natural Language Processing/Python/natural_language_processing.py
@@ -29,10 +29,11 @@ print(corpus)
 
 # Creating the Bag of Words model
 from sklearn.feature_extraction.text import CountVectorizer
-cv = CountVectorizer(max_features = 1500)
-X = cv.fit_transform(corpus).toarray()
-y = dataset.iloc[:, -1].values
+cv = CountVectorizer(max_features = 1500) #Max number of columns. Remember that in English, there are 20.000 words that are common used. But we don't need all of them in this case
+X = cv.fit_transform(corpus).toarray() # Creation of the Matrix of Features, that must be a 2D array
+y = dataset.iloc[:, -1].values # Dependant vector, all rows and just last column
 
+print("Number of tokens: ", len(X[0])) # Use this to refine CountVectorizer max_features parameter
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
@@ -44,10 +45,10 @@ classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
-print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+print("Prediction: \n",np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
-print(cm)
+print("Consufion Matrix: \n",cm)
 accuracy_score(y_test, y_pred)
