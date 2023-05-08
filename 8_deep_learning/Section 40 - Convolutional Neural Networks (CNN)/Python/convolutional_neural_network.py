45693d5cf98d6aa1484e4adc4df4dc643395339a
Antonio Martinez <antonio_martinez88@hotmail.com>
2023-05-08 15:58:11 -0400

    upd

--- 8_deep_learning/Section 40 - Convolutional Neural Networks (CNN)/Python/convolutional_neural_network.py
+++ 8_deep_learning/Section 40 - Convolutional Neural Networks (CNN)/Python/convolutional_neural_network.py
@@ -12,14 +12,14 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
                                    shear_range = 0.2,
                                    zoom_range = 0.2,
                                    horizontal_flip = True)
-training_set = train_datagen.flow_from_directory('dataset/training_set',
+training_set = train_datagen.flow_from_directory('./dataset/training_set',
                                                  target_size = (64, 64),
                                                  batch_size = 32,
                                                  class_mode = 'binary')
 
 # Preprocessing the Test set
 test_datagen = ImageDataGenerator(rescale = 1./255)
-test_set = test_datagen.flow_from_directory('dataset/test_set',
+test_set = test_datagen.flow_from_directory('./dataset/test_set',
                                             target_size = (64, 64),
                                             batch_size = 32,
                                             class_mode = 'binary')
