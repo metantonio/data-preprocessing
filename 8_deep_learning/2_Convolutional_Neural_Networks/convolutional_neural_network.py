0a783ad7f45e8bad8e9773ad73630701f823f633
Antonio Martinez <antonio_martinez88@hotmail.com>
2023-05-09 15:07:30 -0400

    updated

--- 8_deep_learning/2_Convolutional_Neural_Networks/convolutional_neural_network.py
+++ 8_deep_learning/2_Convolutional_Neural_Networks/convolutional_neural_network.py
@@ -3,11 +3,12 @@
 # Importing the libraries
 import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
-tf.__version__
+#import keras.utils as image
+print("Tensorflow version: ",tf.__version__)
 
 # Part 1 - Data Preprocessing
 
-# Preprocessing the Training set
+# Preprocessing the Training set (dividing by 255, is a scale feature of RGB pixels values from 0 to 1 ) other parameters are for augmented 
 train_datagen = ImageDataGenerator(rescale = 1./255,
                                    shear_range = 0.2,
                                    zoom_range = 0.2,
@@ -30,23 +31,23 @@ test_set = test_datagen.flow_from_directory('./dataset/test_set',
 cnn = tf.keras.models.Sequential()
 
 # Step 1 - Convolution
-cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu', input_shape=[64, 64, 3]))
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu', input_shape=[64, 64, 3])) # images are redimensioned to 64x64 pixels and have 3 colors channels. Kernel size = 3 means a feature detector matrix of 3x3
 
 # Step 2 - Pooling
-cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2)) # strides is the 'jump' of the pooling detector to the next column/row. Pool size = 2 means matrix of 2x2
 
 # Adding a second convolutional layer
-cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'))
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu')) # automatically understands image size after 1st convolution and maxpoolin
 cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
 
 # Step 3 - Flattening
 cnn.add(tf.keras.layers.Flatten())
 
 # Step 4 - Full Connection
-cnn.add(tf.keras.layers.Dense(units=128, activation='relu'))
+cnn.add(tf.keras.layers.Dense(units=128, activation='relu')) # Different from ANN. Here, every neuron is connected to all neurons of the next/previous layers
 
 # Step 5 - Output Layer
-cnn.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+cnn.add(tf.keras.layers.Dense(units=1, activation='sigmoid')) # For this example, we can do it in 1 neuron. softmax activation functions is better for multi-classification CNN
 
 # Part 3 - Training the CNN
 
@@ -60,8 +61,8 @@ cnn.fit(x = training_set, validation_data = test_set, epochs = 25)
 
 import numpy as np
 from keras.preprocessing import image
-test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
-test_image = image.img_to_array(test_image)
+test_image = tf.keras.utils.load_img('dataset/test_set/cats/cat.4001.jpg', target_size = (64, 64))
+test_image = tf.keras.utils.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 result = cnn.predict(test_image)
 training_set.class_indices
