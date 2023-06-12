7dcb40b184fc76f0ce6b6a8bee31917688cf5faf
Antonio Martinez <antonio_martinez88@hotmail.com>
2023-06-12 13:26:06 -0400

    upd

--- 10_model_selection_and_boosting/Section 48 - Model Selection/Python/grid_search.py
+++ 10_model_selection_and_boosting/Section 48 - Model Selection/Python/grid_search.py
@@ -29,8 +29,9 @@ classifier.fit(X_train, y_train)
 from sklearn.metrics import confusion_matrix, accuracy_score
 y_pred = classifier.predict(X_test)
 cm = confusion_matrix(y_test, y_pred)
-print(cm)
-accuracy_score(y_test, y_pred)
+print("\nConsusion Matrix:", cm)
+acurracy = accuracy_score(y_test, y_pred)
+print("\nAcurracy = ",acurracy)
 
 # Applying k-Fold Cross Validation
 from sklearn.model_selection import cross_val_score
